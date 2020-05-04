fda292b536a4a22be1108fac4e699a48c4cc31ea
pengw <wenjia.peng@siemens.com>
2020-05-04 17:31:29 +0000

    create a test react component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,7 @@
-import update from './update';
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+import TestComponent from './TestComponent';
 
 // react
-const elem = document.getElementById('react-entry');
-
-
-ReactDOM.render(<h4>Hello React</h4>, elem);
-
-update();
+ReactDOM.render(<TestComponent />, document.getElementById('entry-dom-element'));
