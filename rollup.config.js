03db167e4b19db0b31b165b21092bdc217267da4
pengw <wenjia.peng@siemens.com>
2020-05-04 16:15:38 +0000

    switch to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import serve from 'rollup-plugin-serve';
+import typescript from 'rollup-plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 // `npm run build` -> `production` is true
@@ -8,17 +9,18 @@ import { terser } from 'rollup-plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		file: 'public/bundle.js',
 		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
 		sourcemap: true
 	},
 	plugins: [
+		typescript(),
 		resolve(), // tells Rollup how to find date-fns in node_modules
 		commonjs(), // converts date-fns to ES modules
         production && terser(), // minify, but only in production
-        serve({
+        !production && serve({
             contentBase: 'public',
             host: '0.0.0.0',
             port: 8080
