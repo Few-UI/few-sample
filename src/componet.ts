e0e8ec3bfffd65b9e494b444a052e8d40a076a7a
pengw <wenjia.peng@siemens.com>
2020-05-04 20:49:35 +0000

    prop with types

--- src/componet.ts
+++ src/componet.ts
@@ -6,7 +6,7 @@ interface HighOrderFunc {
 }
 
 export const createComponent = (componentDef: any): HighOrderFunc =>
-    (): JSX.Element => {
+    (props: React.PropsWithChildren<{}>): JSX.Element => {
         const [data, setData] = React.useState(() => componentDef.data());
         return React.createElement(componentDef.view, { data, setData });
     }
