08fed4ed59c4e147ea2b7158948a79f6e91b732f
pengw <wenjia.peng@siemens.com>
2020-05-04 19:39:38 +0000

    typescript lint

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,4 +1,7 @@
-import React, { Component, ReactComponentElement } from 'react';
+/* eslint-env es6 */
+
+import React from 'react';
 
 // https://github.com/typescript-cheatsheets/react-typescript-cheatsheet
-export default ({ value }: { value: string }): JSX.Element => <h4>Hello {value}</h4>;
+const TestComponent = ({ value }: { value: string }): JSX.Element => <h4>Hello {value}</h4>;
+export default TestComponent;
