91080932a50fb1bcd14a7be3ae416c0a3363a760
pengw <wenjia.peng@siemens.com>
2020-05-05 14:09:25 +0000

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,30 @@
 import React from 'react';
 
 export interface ObjectLiteral {
-    [key: string]: ObjectLiteral | string | number | boolean | Function | Array<any>;
+    [key: string]: ObjectLiteral | string | number | boolean | Array<any>;
+}
+
+export interface Action {
+    [key: string]: Function
+}
+
+export interface Component {
+    data: ObjectLiteral,
+    actions: Action,
+    dispatch: Function
 }
 
 export interface ViewTemplate {
-    ({ data, actions, dispatch }: { data: ObjectLiteral, actions: ObjectLiteral, dispatch: Function }): JSX.Element
+    (component: Component): JSX.Element
 }
+
 
 export interface ComponentDefinition {
     view: ViewTemplate,
     data: { (): ObjectLiteral },
     actions: {
         [key: string]: any
-    },
-    actions2?: any
+    }
 }
 
 export interface ComponentFactory {
